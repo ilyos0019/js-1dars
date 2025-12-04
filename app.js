adb52f9cad58c53fa0e64b39f5b10d3f04faa902
ilyos0019 <monyhub0100@gmail.com>
2025-12-04 03:18:30 -0800

    dars2

--- app.js
+++ app.js
@@ -1,10 +1,160 @@
-let x =14;
-let y=16;
+// let x =14;
+// let y=14;
 
-if(x==y){
-    console.log("ikkalsi teng");
-}else if(x>y){
-console.log("x :", x);
-}else{
-    console.log("y :", y);   
+// if(x==y){
+//     console.log("ikkalsi teng");
+// }else if(x>y){
+// console.log("x :", x);
+// }else{
+//     console.log("y :", y);
+// }
+
+
+
+
+// x>y ? console.log("x :", x) : console.log("y :", y);
+
+// x== y ? console.log("ikkalasi ham teng") : x > y ? console.log("x :", x) : console.log("y :", y);
+
+
+// let day1="dushanba"
+// let day2="shanba"
+
+// switch ("seshanba") {
+//     case "dushanba":
+//         console.log(`bugun ${day1}`);
+//         break;
+//     case "shanba":
+//     console.log(`bugun ${day2}`);
+//      break;
+//     default:
+//         console.log(":(");
+//         break;
+// }
+
+
+
+
+
+// let a= 2;
+// let b= -1;
+// let c= -3;
+
+// if(a>=b && b>=c){
+//     a = a*2;
+//     b = b*2;
+//     c = c*2;
+// }else{
+//     a = Math.abs(a)
+//      b = Math.abs(b)
+//       c = Math.abs(c)
+// }
+
+// console.log(a,b,c);
+
+let a = 0.4;
+let b = 0.9;
+let c = 5;
+
+if(a>=1 && a<=3){
+    console.log(a);   
 }
+if(b>=1 && b<=3){
+    console.log(b);
+}
+if(c>=1 && c<=3){
+    console.log(c); 
+}else{
+    console.log("mavjud emas.");  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
